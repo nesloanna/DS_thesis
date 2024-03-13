e3eb6da9ea70ad4d35b4afe313ec7315048b89ba
nesloanna <nesloanna@gmail.com>
2024-03-13 17:45:43 +0100

    Merge and clean bio + meso + nutri

---
+++ data_cleaning_BMN.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import os
+
+os.chdir("/Users/annaolsen/Desktop/Speciale/DS_thesis/data")
+print(os.getcwd())
+
+# Load datasets
+df = pd.read_csv("Tara_merged_BMN.csv")
+
+
+# If you want to reset the index after dropping rows
+df.reset_index(drop=True, inplace=True)
+
+# print(df.columns)
+
+
+new_column_names = [
+    'Sample ID', 'Sample ID (Bio)', 'Sample ID (ENA)', 'Basis', 'Campaign',
+    'Station', 'Method/Device', 'Event', 'Date/Time', 'Latitude', 'Longitude',
+    'Depth layer zone', 'Depth nominal', 'Depth top', 'Depth bot',
+    'Frac lower', 'Frac upper', 'Sample material', 'Sample method',
+    'Sample label', 'MP biome', 'OS region', 'BG province',
+    'Sea ice conc', 'Sea ice free start', 'Sea ice free days',
+    'Sea ice free end', 'Season (spring)', 'Season (early)',
+    'Moon phase nom', 'Moon phase prop', 'Time of day', 'Sunshine duration',
+    'Radiation', 'Radiation 8.1', 'Radiation 8.2', 'Radiation 30',
+    'Sea Surface Temp', 'Iron', 'Iron std', 'Ammonium', 'Ammonium std',
+    'Nitrite', 'Nitrite std', 'Nitrate', 'Nitrate std',
+    'Sea surface fluorescence', 'Sea surface chlorophyll a',
+    'Chlorophyll a', 'Sea surface quantum fluorescence',
+    'Net PP carbon', 'Net PP carbon 30', 'Total suspended matter',
+    'Particulate Organic Carbon', 'Particulate Inorganic Carbon',
+    'Depth bathy', 'Latitude.1', 'Longitude.1', 'Distance closest',
+    'Sea surface temp grad', 'Strain sub-mesoscale', 'u', 'v',
+    'Okubo-Weiss', 'Max Lyapunov Exp', 'Residence time',
+    'Latitude.2', 'Longitude.2', 'Age', 'Latitude.3', 'Longitude.3',
+    'Spec Shannon mean all', 'Spec Shannon month all', 'Spec Shannon mean grp',
+    'Spec Shannon month grp', 'Spec Shannon phy month',
+    'Spec Shannon phy mean', 'Spec miTAG chao', 'Spec miTAG ace',
+    'Spec miTAG shannon', 'Spec richness', 'Functional diversity',
+    'Functional richness', 'Functional evenness', 'Distance interval',
+    'Duration (ISO)', 'No of observations', 'Phosphate min', 'Phosphate lower',
+    'Phosphate median', 'Phosphate upper', 'Phosphate max'
+]
+
+
+df.columns = new_column_names
+
+# print(df.columns)
+
+# Removing the ID pattern from OS region and BG province
+ID_pattern = r'\(MRGID:(\d+)\)'
+
+df['OS region'] = df['OS region'].str.replace(ID_pattern, '', regex=True)
+df['BG province'] = df['BG province'].str.replace(ID_pattern, '', regex=True)
+
+
+# Removing the ENVO pattern from Depth layer zone
+ENVO_pattern = r'\(ENVO:(\d+)\)'
+
+df['Depth layer zone'] = df['Depth layer zone'].str.replace(
+    ENVO_pattern, '', regex=True)
+
+
+# Define your replacements in a dictionary
+depth_layer_replace = {
+    '[SRF] surface water layer ': '[SRF] Surface Water',
+    '[DCM] deep chlorophyll maximum layer ': '[DCM] Deep Chlorophyll Maximum',
+    '[ZZZ] marine water layer ': '[ZZZ] Marine Water',
+    '[MES] marine water layer  within the mesopelagic zone ': '[MES] Marine Water Mesopelagic Zone',
+    '[MIX] marine epipelagic wind mixed layer ': '[MIX] Marien Epipelagic Wind Mixed',
+    '[FSW] filtered sea water, used to control protocols': '[FSW] Filtered Sea Water',
+}
+
+# Replace values using the dictionary
+df['Depth layer zone'] = df['Depth layer zone'].replace(depth_layer_replace)
+
+
+# Convert the 'DateTime' column to datetime format
+df['Date/Time'] = pd.to_datetime(df['Date/Time'])
+
+# Extract date and year into new columns
+df['Date'] = df['Date/Time'].dt.date
+df['Year'] = df['Date/Time'].dt.year
+
+
+# # Sort all columns (but Sample ID) alphabetically
+# columns_to_sort = sorted(
+#     [col for col in df.columns if col not in ['Sample ID']])
+
+# # Reorder the columns
+# desired_columns = ['Sample ID'] + columns_to_sort
+
+# # Create a new dataframe with the desired column order, sorted alphabetically
+# df = df[desired_columns]
+
+
+df.to_csv("Tara_BMN_Cleaned.csv", index=False)
--- merge_bio_meso.py
+++ merge_bio_meso.py
@@ -7,13 +7,13 @@ print(os.getcwd())
 
 # Load datasets
 df_bio = pd.read_csv("Tara_Biodiversity.csv")
-df_depth = pd.read_csv("Tara_Environmental_Depth.csv")
 df_meso = pd.read_csv("Tara_Env_Meso_SampleLocation.csv")
+df_nutri = pd.read_csv("Tara_Env_Nut.csv")
 
 # New column names for Meso SL
 new_column_names = {
     'Sample ID (TARA_barcode#, registered at ...)': 'Sample ID (TARA_barcode#)',
-    'Station (TARA_station#, registered at ...)': 'Station (TARA_station#)'
+    'Station (TARA_station#, registered at ...)': 'Station'
 }
 
 # Rename the columns
@@ -22,20 +22,21 @@ df_meso = df_meso.rename(columns=new_column_names)
 # Find unique sample IDs
 ids_df_bio = set(df_bio["Sample ID (TARA_barcode#)"])
 ids_df_meso = set(df_meso["Sample ID (TARA_barcode#)"])
-ids_df_depth = set(df_depth["Sample ID (TARA_barcode#)"])
+ids_df_nutri = set(df_nutri['Sample ID'])
 
 # Find the Sample IDs that are not common between the DataFrames
 unique_ids_bio = ids_df_bio - ids_df_meso
 unique_ids_meso = ids_df_meso - ids_df_bio
-unique_ids_depth = ids_df_depth - ids_df_meso
+unique_ids_nutri = ids_df_nutri - ids_df_meso
 
 print("Unique IDs bio:", len(unique_ids_bio))
 print("Unique IDs meso:", len(unique_ids_meso))
-print("Unique IDs depth:", len(unique_ids_depth))
-
+print("Unique IDs nutri:", len(unique_ids_nutri))
+print()
 print("Original shape of bio:", df_bio.shape)
 print("Original shape of meso:", df_meso.shape)
-
+print("Original shape of nutri:", df_nutri.shape)
+print()
 
 # Filter unique IDs from df_bio
 unique_df_bio = df_bio[df_bio["Sample ID (TARA_barcode#)"].isin(
@@ -45,36 +46,22 @@ unique_df_bio = df_bio[df_bio["Sample ID (TARA_barcode#)"].isin(
 unique_df_meso = df_meso[df_meso["Sample ID (TARA_barcode#)"].isin(
     unique_ids_meso)]
 
-# Filter unique IDs from df_meso
-unique_df_depth = df_depth[df_depth["Sample ID (TARA_barcode#)"].isin(
-    unique_ids_depth)]
-
 # Display the lengths of the unique dataframes
 print("Shape of unique bio df:", unique_df_bio.shape)
 print("Shape of unique meso df:", unique_df_meso.shape)
-print("Shape of unique depth df:", unique_df_depth.shape)
 
-# unique_df_bio.to_csv("Bio_Unique.csv", index=False)
-# unique_df_meso.to_csv("Meso_Unique.csv", index=False)
-# unique_df_depth.to_csv("Depth_Unique.csv", index=False)
-
-columns_to_keep = ['Sample ID (TARA_barcode#)',
-                   '[PO4]3- [µmol/l] (in the selected environmental...)']
-df_depth = df_depth[columns_to_keep]
 
 # ------- Add extra IDs -------
 
-new_ids_for_bio = sorted(list(unique_ids_meso))
-df_new_bio_ids = pd.DataFrame(
-    {'Sample ID (TARA_barcode#)': new_ids_for_bio})
+# new_ids_for_bio = sorted(list(unique_ids_meso))
+# df_new_bio_ids = pd.DataFrame(
+#     {'Sample ID (TARA_barcode#)': new_ids_for_bio})
 
-df_bio = pd.merge(df_bio, df_depth,
-                  on='Sample ID (TARA_barcode#)', how="inner")
 
-df_bio = pd.concat([df_bio, df_new_bio_ids], ignore_index=True)
-df_bio = df_bio.sort_values(by='Sample ID (TARA_barcode#)')
+# df_bio = pd.concat([df_bio, df_new_bio_ids], ignore_index=True)
+# df_bio = df_bio.sort_values(by='Sample ID (TARA_barcode#)')
 
-print("Shape (bio) after adding meso IDs:", df_bio.shape)
+# print("Shape (bio) after adding meso IDs:", df_bio.shape)
 
 
 # Create a boolean mask where IDs are in the list of IDs to remove
@@ -89,7 +76,7 @@ df_bio = df_bio.reset_index(drop=True)
 print("Shape (bio) after removing unique IDs:", df_bio.shape)
 
 # Initialize a list to store the names of identical columns
-identical_columns = []
+# identical_columns = []
 
 # # Iterate over the columns of df_bio
 # for col in df_bio.columns:
@@ -117,22 +104,37 @@ df_bio = df_bio.drop(columns=columns_to_drop)
 duplicate_cols = ['Sample ID (TARA_barcode#)']
 
 # Merge df_bio and df_meso
-df_merged = pd.merge(df_meso, df_bio, on=duplicate_cols, how="inner")
+df_merged = pd.merge(df_meso, df_bio, on=duplicate_cols, how="outer")
 
 print("Shape after merging bio and meso:", df_merged.shape)
 
-# # df_merged = df_merged.sort_values(by='Sample ID (TARA_barcode#)')
-# # df_meso_SL = df_meso_SL.sort_values(by='Sample ID (TARA_barcode#)')
+cols_to_drop_nutri = [
+    'Sample ID (Bio)', 'Sample ID (ENA)', 'Station', 'Event',
+    'Env feature', 'Depth nominal', 'Depth top/min', 'Depth bot/max',
+    'Size frac lower', 'Size frac upper', 'Sample material',
+    'Sample method', 'Sample code/label', 'File name', 'Nitrite min',
+    'Nitrite lower', 'Nitrite median', 'Nitrite upper', 'Nitrite max',
+    'Nitrate/Nitrite min', 'Nitrate/Nitrite lower', 'Nitrate/Nitrite median',
+    'Nitrate/Nitrite upper', 'Nitrate/Nitrite max', 'Silicate min',
+    'Silicate lower', 'Silicate median', 'Silicate upper', 'Silicate max'
+]
 
-# duplicate_cols_meso = ['Sample ID (TARA_barcode#)', 'Campaign',
-#                        'Station (TARA_station#)', 'Event', 'Basis',
-#                        'Date/Time', 'Method/Device']
+df_nutri = df_nutri.drop(columns=cols_to_drop_nutri)
 
-# # Merge df_merged and df_meso
-# df_merged = pd.merge(df_meso, df_merged, on='Sample ID (TARA_barcode#)',
-#                      how="outer", suffixes=['_D', '_M'])
 
-# print(df_merged.shape)
+# Rename the columns
+df_merged = df_merged.rename(
+    columns={'Sample ID (TARA_barcode#)': 'Sample ID'})
+
+
+df_merged = pd.merge(df_merged, df_nutri, on='Sample ID', how="inner")
+
+print("Shape after merging bio/meso with nutri:", df_merged.shape)
+
+
+df_merged.to_csv("Tara_merged_BMN.csv", index=False)
+
+# df_merged = df_merged.sort_values(by='Sample ID (TARA_barcode#)')
 
 
 # # Sort all columns (but Sample ID) alphabetically
